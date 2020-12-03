686c3d0b52705aa0894d2116f7f48a464957c075
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-12-03 19:20:59 -0400

    Collect, parse, and return ISS flyover times.

--- index.js
+++ index.js
@@ -24,10 +24,20 @@
 // });
 
 const { nextISSTimesForMyLocation } = require('./iss');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (!error) {
     if (passTimes === null) return; // Hold if no data on initial request (works in async scenarios in particular)
-    console.log(passTimes);
+    printPassTimes(passTimes);
   } else {
     console.log('Error', error);
   }
